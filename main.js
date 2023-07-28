16ef2250f6bf4dc8412dc2122a4f1145f60f347e
PaingHeinKhant <paingheinkhant.mm@gmail.com>
2023-07-29 05:56:24 +0630

    add dishes

--- main.js
+++ main.js
@@ -1,130 +1,188 @@
-import './style.scss'
-import Typed from 'typed.js';
+import "./style.scss";
+import Typed from "typed.js";
 import ScrollReveal from "scrollreveal";
 import "waypoints/lib/noframework.waypoints.min";
 
-
 let options = {
-    strings: ['Food ...', 'Drink ...'],
-    typeSpeed: 30,
-    backSpeed : 30,
-    backDelay:3000,
-    loop:true
+  strings: ["Food ...", "Drink ..."],
+  typeSpeed: 30,
+  backSpeed: 30,
+  backDelay: 3000,
+  loop: true,
 };
 
-let typed = new Typed('.element', options);
-
+let typed = new Typed(".element", options);
 
 let toDown = {
-    distance: '50px',
-    origin: 'top',
-    interval : 300,
-    duration : 800,
+  distance: "50px",
+  origin: "top",
+  interval: 300,
+  duration: 800,
 };
 
-ScrollReveal().reveal('.to-down', toDown);
+ScrollReveal().reveal(".to-down", toDown);
 
 let toRight = {
-    distance: '50px',
-    origin: 'left',
-    interval : 300,
-    duration : 800,
+  distance: "50px",
+  origin: "left",
+  interval: 300,
+  duration: 800,
 };
 
-ScrollReveal().reveal('.to-right', toRight);
+ScrollReveal().reveal(".to-right", toRight);
 
 let toLeft = {
-    distance: '50px',
-    origin: 'right',
-    interval : 300,
-    duration : 800,
+  distance: "50px",
+  origin: "right",
+  interval: 300,
+  duration: 800,
 };
 
-ScrollReveal().reveal('.to-left', toLeft);
-
+ScrollReveal().reveal(".to-left", toLeft);
 
 new Waypoint({
-    element: document.getElementById('home-content'),
-    handler: function(direction) {
-        let oldNavLink = document.querySelector(".nav-link.active");
-        if(oldNavLink != null){
-            oldNavLink.classList.remove("active");
-        }
-        let currentNavLink = document.querySelector(`[href="#home"]`);
-        currentNavLink.classList.add('active');
-    },
-    offset: "10%"
+  element: document.getElementById("home-content"),
+  handler: function (direction) {
+    let oldNavLink = document.querySelector(".nav-link.active");
+    if (oldNavLink != null) {
+      oldNavLink.classList.remove("active");
+    }
+    let currentNavLink = document.querySelector(`[href="#home"]`);
+    currentNavLink.classList.add("active");
+  },
+  offset: "10%",
 });
 
-
-let sections = ['about','services','menus'];
-sections.forEach(function (section){
-    new Waypoint({
-        element: document.getElementById(section),
-        handler: function(direction) {
-            let oldNavLink = document.querySelector(".nav-link.active");
-            oldNavLink.classList.remove("active");
-            let currentNavLink = document.querySelector(`[href="#${section}"]`);
-            currentNavLink.classList.add('active');
-        },
-        offset: "50%"
-    })
-})
-
+let sections = ["about", "services", "menus"];
+sections.forEach(function (section) {
+  new Waypoint({
+    element: document.getElementById(section),
+    handler: function (direction) {
+      let oldNavLink = document.querySelector(".nav-link.active");
+      oldNavLink.classList.remove("active");
+      let currentNavLink = document.querySelector(`[href="#${section}"]`);
+      currentNavLink.classList.add("active");
+    },
+    offset: "50%",
+  });
+});
 
 new Waypoint({
-    element: document.getElementById('contact'),
-    handler: function(direction) {
-        let oldNavLink = document.querySelector(".nav-link.active");
-        oldNavLink.classList.remove("active");
-        let currentNavLink = document.querySelector(`[href="#contact"]`);
-        currentNavLink.classList.add('active');
-    },
-    offset: "80%"
-})
-
+  element: document.getElementById("contact"),
+  handler: function (direction) {
+    let oldNavLink = document.querySelector(".nav-link.active");
+    oldNavLink.classList.remove("active");
+    let currentNavLink = document.querySelector(`[href="#contact"]`);
+    currentNavLink.classList.add("active");
+  },
+  offset: "80%",
+});
 
 let menuLists = [
-    {
-        id:1,
-        img : "public/img/plate1.png",
-        title:"Barbecue Salad",
-        description : "Special Delicious Dish",
-        price : 22.2,
-        currency : "$"
-    },
-    {
-        id:2,
-        img : "public/img/plate2.png",
-        title:"Salad with fish",
-        description : "Special Delicious Dish",
-        price : 30.5,
-        currency : "$"
-    },
-    {
-        id:3,
-        img : "public/img/plate3.png",
-        title:"Spinach Salad",
-        description : "Special Delicious Dish",
-        price : 50,
-        currency : "$"
-    },
-    {
-        id:4,
-        img : "public/img/plate3.png",
-        title:"San Kyi Tar",
-        description : "Special Delicious Dish",
-        price : 50,
-        currency : "$"
-    }
+  {
+    id: 1,
+    img: "public/img/plate1.png",
+    title: "Barbecue Salad",
+    description: "Special Delicious Dish",
+    price: 22.2,
+    currency: "$",
+  },
+  {
+    id: 2,
+    img: "public/img/plate2.png",
+    title: "Salad with fish",
+    description: "Special Delicious Dish",
+    price: 30.5,
+    currency: "$",
+  },
+  {
+    id: 3,
+    img: "public/img/plate3.png",
+    title: "Spinach Salad",
+    description: "Special Delicious Dish",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 4,
+    img: "public/img/plate1.png",
+    title: "San Kyi Tar",
+    description: "Special Delicious Dish",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 5,
+    img: "public/img/plate2.png",
+    title: "San Food",
+    description: "Special Delicious Dish",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 6,
+    img: "public/img/plate3.png",
+    title: "Kyi Food",
+    description: "Special Delicious Dish",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 7,
+    img: "public/img/plate2.png",
+    title: "Tar Food",
+    description: "Dish",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 8,
+    img: "public/img/plate1.png",
+    title: "San w Food",
+    description: "Special Delicious ",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 9,
+    img: "public/img/plate3.png",
+    title: "San w Food",
+    description: "Special Delicious ",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 10,
+    img: "public/img/plate2.png",
+    title: "San w Food",
+    description: "Special Delicious ",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 11,
+    img: "public/img/plate1.png",
+    title: "San w Food",
+    description: "Special Delicious ",
+    price: 50,
+    currency: "$",
+  },
+  {
+    id: 12,
+    img: "public/img/plate3.png",
+    title: "San w Food",
+    description: "Special Delicious ",
+    price: 50,
+    currency: "$",
+  },
 ];
 
-let menuRow = document.getElementById('menuRow');
+let menuRow = document.getElementById("menuRow");
 
-menuLists.forEach(function (menuList){
-    let div = document.createElement('div');
-    div.classList.add("col-8","col-md-4","col-lg-2");
-    div.innerHTML = `
+menuLists.forEach(function (menuList) {
+  let div = document.createElement("div");
+  div.classList.add("col-8", "col-md-4", "col-lg-2");
+  div.innerHTML = `
         <div class="card plate-card mb-5 ">
             <img src="${menuList.img}" class="w-75 mx-auto d-block my-4" alt="">
             <div class="card-body">
@@ -142,5 +200,5 @@ menuLists.forEach(function (menuList){
         </div>
     `;
 
-    menuRow.append(div)
-})
+  menuRow.append(div);
+});
